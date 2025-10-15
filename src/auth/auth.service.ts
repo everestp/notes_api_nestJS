288bb3e641e2473d77f863317b7e41ff325e3d44
everestp <paudeleverest09@gmail.com>
2025-10-15 19:20:36 +0545

    Login EndPoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,39 +1,70 @@
-import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import { ConflictException, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { RegistrerDto } from './dto/register.dto';
 import { UserService } from 'src/user/user.service';
 import bcrypt from 'bcrypt'
 import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
 @Injectable()
 export class AuthService {
     private readonly logger = new Logger(AuthService.name)
     constructor(
-        private readonly userService:UserService,
-        private readonly jwtService:JwtService
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService
 
-    ){}
-    async  register(registrerDto:RegistrerDto){
-       const user =  await   this.userService.getUserByEmail(registrerDto.email)
-       //check email  or username is already exist or not
-       if(user){
-        throw new ConflictException("Email already Taken ")
-       }
+    ) { }
+    async register(registrerDto: RegistrerDto) {
+        const user = await this.userService.getUserByEmail(registrerDto.email)
+        //check email  or username is already exist or not
+        if (user) {
+            throw new ConflictException("Email already Taken ")
+        }
 
-       // hash the passowrd
-  const saltRound = 10;
-  const hashedPassword =  await bcrypt.hash(registrerDto.password ,saltRound);
-     const newUser =   await  this.userService.createuser({...registrerDto ,password:hashedPassword})
-     this.logger.log(`New user  is Created  :",${newUser.id}`)
-     // generate JWt token
+        // hash the passowrd
+        const saltRound = 10;
+        const hashedPassword = await bcrypt.hash(registrerDto.password, saltRound);
+        const newUser = await this.userService.createuser({ ...registrerDto, password: hashedPassword })
+        this.logger.log(`New user  is Created  :",${newUser.id}`)
+        // generate JWt token
 
-const payload = {sub:newUser.id ,email:newUser.email }
-  const token = await  this.jwtService.signAsync(payload)
-   return {token}
+        const payload = { sub: newUser.id, email: newUser.email }
+        const token = await this.jwtService.signAsync(payload)
+        return { token }
 
     }
 
 
-    getUserByEmail(registrerDto:RegistrerDto){
-        return this.userService.getUserByEmail(registrerDto.email)
+    async loginUser(loginDto: LoginDto) {
+        /**
+         * 
+         *1. GEt the user from the db
+         2 . Match the password with hash passwword
+         3. Create JWT token
+         4. Return JWT token
+         */
+
+        const user = await this.userService.getUserByEmail(loginDto.email)
+        if (!user) {
+            throw new UnauthorizedException("Email or password is incorrect")
+        }
+        const isMatch = await bcrypt.compare(loginDto.password, user.password)
+
+        //DOn tell which one is in correct just tell genetic message every time that passwprd or email is incorecct dont say email not exist 
+        if (!isMatch) {
+            throw new UnauthorizedException("Email or password is incorrect")
+        }
+        const payload = { sub: user.id, email: user.email }
+        const token = await this.jwtService.signAsync(payload)
+        return {
+            message: "Login Successfull",
+            suscess: true,
+            data: token
+
+        }
+
     }
+
+
+
+
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from 'src/auth/dto/login.dto';
 import { RegistrerDto } from 'src/auth/dto/register.dto';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
     constructor(private readonly prismaService:PrismaService){}
-     async getUserByEmail(email:string){
-        //call databases  and check the email 
-       const user =   await  this.prismaService.user.findFirst({where :{email}})
-        return user
-    }
 
 
     async createuser(registerDto:RegistrerDto){
-        return await this.prismaService.user.create({data:registerDto})    }
+        return await this.prismaService.user.create({data:registerDto})  
+    
+        
+    }
+    async getUserByEmail(email:string){
+        return this.prismaService.user.findFirst({where:{email}})
+    }
 } 
